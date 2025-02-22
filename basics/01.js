0c50cb2ba16dfbafb37c8b8d01e86a02753b651f
anumair <153544340+anumair@users.noreply.github.com>
2025-02-22 17:24:52 +0000

    configure

--- basics/01.js
+++ basics/01.js
@@ -1,1 +1,13 @@
-console.log("hello")
+
+console.log("he");
+// for running node foldername/filename.js
+let a=123;
+var c="hihi";
+//; can be applies at last or not no difference
+//constant variable value cant be changed
+constant thesis="aknx"
+
+let d 
+d=1256
+console.table([a,c,d]);
+//printg as table
