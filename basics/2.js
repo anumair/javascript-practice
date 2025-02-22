0f22fc8e45e81a04f0fb4849426740088911884c
anumair <153544340+anumair@users.noreply.github.com>
2025-02-22 17:24:30 +0000

    configure

---
+++ basics/2.js
@@ -0,0 +1,6 @@
+//use strict all things will be running according to newer version 
+//alert() we are using node.js not javascript
+//mdn website can be used for syntax
+//null,undefined value
+let a=123
+console.log(typeof(a))
